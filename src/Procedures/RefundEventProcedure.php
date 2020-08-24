7ea014fba3a4d8e558343bc3877f6d4f43da6e73
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2020-08-24 14:19:32 +0530

    Update RefundEventProcedure.php

--- src/Procedures/RefundEventProcedure.php
+++ src/Procedures/RefundEventProcedure.php
@@ -153,7 +153,7 @@ class RefundEventProcedure
 						$paymentData['mop']         = $paymentDetails[0]->mopId;
 						$paymentData['booking_text'] = $transactionComments;  
 						$this->paymentHelper->updatePayments($paymentData['tid'], $responseData['tid_status'], $order->id);
-						$this->paymentHelper->createPlentyPayment($paymentData);
+						//$this->paymentHelper->createPlentyPayment($paymentData);
 					}
 
 				} else {
