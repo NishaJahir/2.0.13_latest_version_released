41c3ecf7bda64f785985cb4c94fa81e7320e9f23
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2020-08-25 12:26:23 +0530

    Update PaymentHelper.php

--- src/Helper/PaymentHelper.php
+++ src/Helper/PaymentHelper.php
@@ -605,15 +605,15 @@ class PaymentHelper
      * @param int $orderId
      * @return null
      */
-    public function updatePayments($tid, $tid_status, $orderId, $refund_process=false, $partial_refund=false)
+    public function updatePayments($tid, $tid_status, $orderId)
     {    
-        $paymentCreate = pluginApp(\Plenty\Modules\Payment\Models\Payment::class);
+        //$paymentCreate = pluginApp(\Plenty\Modules\Payment\Models\Payment::class);
         $payments = $this->paymentRepository->getPaymentsByOrderId($orderId);
         foreach ($payments as $payment) {
-         if($refund_process) {
-         $payment->status = ($partial_refund == true) ? Payment::STATUS_PARTIALLY_REFUNDED : Payment::STATUS_REFUNDED;
-         $payment->updateOrderPaymentStatus = true;
-         }
+         //if($refund_process) {
+        // $payment->status = ($partial_refund == true) ? Payment::STATUS_PARTIALLY_REFUNDED : Payment::STATUS_REFUNDED;
+         //$payment->updateOrderPaymentStatus = true;
+         //}
         $paymentProperty     = [];
         $paymentProperty[]   = $this->getPaymentProperty(PaymentProperty::TYPE_BOOKING_TEXT, $tid);
         $paymentProperty[]   = $this->getPaymentProperty(PaymentProperty::TYPE_TRANSACTION_ID, $tid);
