8ecc8b91d1f82d6b43b1f8e333c46d1e4d7df567
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2020-08-25 10:21:44 +0530

    Update PaymentHelper.php

--- src/Helper/PaymentHelper.php
+++ src/Helper/PaymentHelper.php
@@ -205,6 +205,7 @@ class PaymentHelper
          if(!empty($requestData['type']) && $requestData['type'] == 'debit')
         {
             $payment->type = $requestData['type'];
+             $payment->unaccountable = 0;
             $payment->status = ($partial_refund == true )  ? Payment::STATUS_PARTIALLY_REFUNDED : Payment::STATUS_REFUNDED;
         }
         
