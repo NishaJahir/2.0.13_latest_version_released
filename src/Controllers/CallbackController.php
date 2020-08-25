66661f1af2e4c43673bbf77c15c6cb38403f427e
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2020-08-25 10:20:16 +0530

    Update CallbackController.php

--- src/Controllers/CallbackController.php
+++ src/Controllers/CallbackController.php
@@ -381,8 +381,8 @@ class CallbackController extends Controller
 			    
 		  }
 
-                //$this->paymentHelper->createPlentyPayment($paymentData, $partial_refund_amount);
-		$this->paymentHelper->updatePayments($this->aryCaptureParams['tid'], $this->aryCaptureParams['tid_status'], $nnTransactionHistory->orderNo);
+                $this->paymentHelper->createPlentyPayment($paymentData, $partial_refund_amount);
+		//$this->paymentHelper->updatePayments($this->aryCaptureParams['tid'], $this->aryCaptureParams['tid_status'], $nnTransactionHistory->orderNo);
                 $this->sendCallbackMail($callbackComments);
                 return $this->renderTemplate($callbackComments);
             }
@@ -425,7 +425,7 @@ class CallbackController extends Controller
                 }  elseif (in_array($this->aryCaptureParams['payment_type'], ['CREDITCARD', 'INVOICE_START', 'GUARANTEED_INVOICE', 'DIRECT_DEBIT_SEPA', 'GUARANTEED_DIRECT_DEBIT_SEPA'] )) {
                 
                     $transactionStatus = $this->payment_details($nnTransactionHistory->orderNo);
-		
+		if ($this->aryCaptureParams['tid_status'] !=  $transactionStatus && (in_array($this->aryCaptureParams['tid_status'], ['91', '99', '100']) && in_array($transactionStatus, ['75', '91', '98', '99']))) {
                     $saveAdditionData = false;
 			
 			if ($this->aryCaptureParams['tid_status'] == '100' && $transactionStatus == '98') {
@@ -497,7 +497,10 @@ class CallbackController extends Controller
                      if (!empty($callbackComments)) {
 			$this->sendCallbackMail($callbackComments); 
 			return $this->renderTemplate($callbackComments);
-			} 
+			}
+		}
+			$error = 'Novalnet Callbackscript received. Payment type ( '.$this->aryCaptureParams['payment_type'].' ) is not applicable for this process!';
+			return $this->renderTemplate($error);
 			  
                 }  elseif('PRZELEWY24' == $this->aryCaptureParams['payment_type'] && (!in_array($this->aryCaptureParams['tid_status'], ['100','86']) || '100' != $this->aryCaptureParams['status'])){
                     // Przelewy24 cancel.
