7e61302d8d5dd24fe60a29bb02ab583e515a852c
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2020-09-08 15:21:01 +0530

    Update NovalnetPaymentProcessButton.php

--- src/Providers/NovalnetPaymentProcessButton.php
+++ src/Providers/NovalnetPaymentProcessButton.php
@@ -16,3 +16,17 @@ use Novalnet\Services\TransactionService;
 use Plenty\Plugin\Http\Request;
 use Plenty\Plugin\Http\Response;
 
+class NovalnetPaymentProcessButton {
+  public function call(Twig $twig, PaymentRepositoryContract $paymentRepositoryContract, Request $request, Response $response, $arg) {
+    $paymentHelper = pluginApp(PaymentHelper::class);
+        $paymentService = pluginApp(PaymentService::class);
+        $transactionLog  = pluginApp(TransactionService::class); 
+        $sessionStorage = pluginApp(FrontendSessionStorageFactoryContract::class);
+        $order = $arg[0];
+      $paymentHelper->logger('order', $order);
+    $paymentHelper->logger('request', $request->all());
+      $paymentHelper->logger('argument', $arg); 
+      return $twig->render('Novalnet::NovalnetPaymentButton', ['request' => 'test']);
+  }
+  
+}
