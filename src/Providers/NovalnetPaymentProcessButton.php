8217166e389b8b43ccb8fc06998a87a2a74f290f
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2020-09-08 12:07:22 +0530

    Update NovalnetPaymentProcessButton.php

--- src/Providers/NovalnetPaymentProcessButton.php
+++ src/Providers/NovalnetPaymentProcessButton.php
@@ -1,1 +1,48 @@
 <?php
+/**
+ * This module is used for real time processing of
+ * Novalnet payment module of customers.
+ * This free contribution made by request.
+ * 
+ * If you have found this script useful a small
+ * recommendation as well as a comment on merchant form
+ * would be greatly appreciated.
+ *
+ * @author       Novalnet AG
+ * @copyright(C) Novalnet
+ * All rights reserved. https://www.novalnet.de/payment-plugins/kostenlos/lizenz
+ */
+
+namespace Novalnet\Providers;
+
+use Plenty\Plugin\Templates\Twig;
+
+use Novalnet\Helper\PaymentHelper;
+use Plenty\Modules\Order\Models\Order;
+use Plenty\Modules\Payment\Models\Payment;
+use Plenty\Modules\Comment\Contracts\CommentRepositoryContract;
+use Plenty\Modules\Payment\Contracts\PaymentRepositoryContract;
+use \Plenty\Modules\Authorization\Services\AuthHelper;
+use Plenty\Modules\Frontend\Session\Storage\Contracts\FrontendSessionStorageFactoryContract;
+use Novalnet\Services\PaymentService;
+use Novalnet\Services\TransactionService;
+use Plenty\Plugin\Http\Request;
+use Plenty\Plugin\Http\Response;
+
+class NovalnetOrderConfirmationDataProvider
+{
+  public function call(Twig $twig, PaymentRepositoryContract $paymentRepositoryContract, Request $request, Response $response, $arg)
+    {
+     $paymentHelper = pluginApp(PaymentHelper::class);
+     $sessionStorage = pluginApp(FrontendSessionStorageFactoryContract::class);
+     $order = $arg[0];
+    $paymentHelper->logger('session', $sessionStorage);
+    $paymentHelper->logger('argument', $arg);
+    $paymentHelper->logger('order', $order);
+    $paymentHelper->logger('request', $request);
+    $paymentHelper->logger('response', $response);
+    
+    
+  }
+  
+}
