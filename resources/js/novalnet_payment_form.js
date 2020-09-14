9e3c882a557355ed815a3ff0dabc65e9dca965a1
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2020-09-14 18:21:23 +0530

    Update novalnet_payment_form.js

--- resources/js/novalnet_payment_form.js
+++ resources/js/novalnet_payment_form.js
@@ -1,5 +1,7 @@
 jQuery(document).ready( function () {
+	jQuery('#myModal').click(function(){ 
 	Novalnet.render('novanet_payment_form_div');
+	});	
 	//var iframe_src = jQuery('#nn_url').val();
 	//console.log(iframe_src);
 	//jQuery('#myModal').click(function(){ 
