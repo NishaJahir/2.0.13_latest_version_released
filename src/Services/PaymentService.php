098d08619732f8fc936fbbe255559b882c9a3ccf
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2020-09-14 15:46:52 +0530

    Update PaymentService.php

--- src/Services/PaymentService.php
+++ src/Services/PaymentService.php
@@ -926,10 +926,11 @@ class PaymentService
 		"invoice_type" => "INVOICE",
 		"amount" => 10,
 	        'uniqid' => $this->paymentHelper->getUniqueId(),
-		'return_url' => $this->getReturnPageUrl(),
-			'error_return_url' => $this->getReturnPageUrl(),
-			'return_method' => 'POST',
-			'error_return_method' => 'POST'
+		'sfid' => 
+		//'return_url' => $this->getReturnPageUrl(),
+			//'error_return_url' => $this->getReturnPageUrl(),
+			//'return_method' => 'POST',
+			//'error_return_method' => 'POST'
 		];
      		$this->encodePaymentData($paymentRequestData);
 		return [
