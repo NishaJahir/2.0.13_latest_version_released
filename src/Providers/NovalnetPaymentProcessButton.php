76630b38eb02c3030c4dcc1c608a0099522dc6ef
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2020-09-14 18:30:55 +0530

    Update NovalnetPaymentProcessButton.php

--- src/Providers/NovalnetPaymentProcessButton.php
+++ src/Providers/NovalnetPaymentProcessButton.php
@@ -37,7 +37,7 @@ class NovalnetPaymentProcessButton {
       $paymentHelper->logger('argument', $arg); 
     $paymentHelper->logger('basket', $basket); 
     $paymentHelper->logger('payment details', $payments); 
-      return $twig->render('Novalnet::NovalnetPaymentButton', ['request' => $getRequest ]);
+      return $twig->render('Novalnet::NovalnetPaymentButton', ['request' => $getRequest['data'] ]);
     
    
    // return $responseAll->redirectTo($responseData['url']);
